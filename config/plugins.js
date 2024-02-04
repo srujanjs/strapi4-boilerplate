1412daa2703792f9fae2d7b1e1cedd424338c755
Srujan Gopu <sgopu-nonemp@collegeboard.org>
2024-02-04 13:14:15 -0800

    initial commit

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = {
+  default: ({ env }) => ({
+    jwtSecret: env("JWT_SECRET"),
+    jwt: {
+      expiresIn: "30d",
+    },
+    ratelimit: {
+      interval: 60000,
+      max: 10,
+    },
+  }),
+};
